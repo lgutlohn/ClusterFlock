ab1cde14a6c3e08e718a2857afa2b6628c519011
Kathleen <blackrosestar333@gmail.com>
2014-09-28 11:41:33 -0400

    Test code for drag & drop functionality to work. JavaScript and CSS connect. Using Jade. Addition to Lauren's Initial Set up.

---
+++ n/public/javascript/test.js
@@ -0,0 +1,15 @@
+alert("I'm in test javascript");
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text/html", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text/html");
+    ev.target.appendChild(document.getElementById(data));
+}
