72849aa881291dce524e9bb90a27320eb6d25b76
Eric Smith <ericsmith1302@gmail.com>
2014-10-15 14:10:15 -0400

    Implemented drag and drop with the new library

--- frontEnd/public/js/controllers/MainCtrl.js
+++ frontEnd/public/js/controllers/MainCtrl.js
@@ -1,5 +1,9 @@
-angular.module('MainCtrl', []).controller('MainController', ['$scope', 'TweetService',
+angular.module('MainCtrl', ['ngDraggable']).controller('MainController', ['$scope', 'TweetService',
 	function($scope, TweetService) {
+		$scope.onDropComplete=function(data,evt){
+	       console.log(data);
+	    }
+
 		$scope.updateTweets = function() {
 			TweetService.get($scope.name).then(function(dataResponse) {
 		        $scope.tweets = dataResponse.data;
