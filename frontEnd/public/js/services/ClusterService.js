fb468488b946f99d79bc5bad8b80e8dfcfa06ce4
Jesse Everett <jesseceverett@gmail.com>
2014-10-31 13:26:54 -0400

    Update ClusterService.js
    added new functinality

--- frontEnd/public/js/services/ClusterService.js
+++ frontEnd/public/js/services/ClusterService.js
@@ -2,13 +2,21 @@ angular.module('ClusterService', []).factory('ClusterService', ['$http', functio
 
 	return {
 		
-		save : function(data) {
-			return $http.get('/api/saveCluster?name='+data.name+'&description='+data.description);
+		save : function(name, description) {
+			return $http.get('/api/saveCluster?name='+name+'&description='+description);
 		},
 
 		getClusters : function() {
 			return $http.get('/api/getClusters');
 		},
+
+		getClusterName : function(data){
+			return $http.get('/api/saveCluster?name='+data.name);
+		},
+
+		getDescription : function(data){
+			return $http.get('/api/saveCluster?description='+data.description);
+		}
 	}	
 
 }]);
