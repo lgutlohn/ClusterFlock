c736137513ab050a2b234e145ffd919912c8a404
Jesse Everett <jesseceverett@gmail.com>
2014-10-31 13:27:22 -0400

    Update cluster_test.js
    Added new tests

--- frontEnd/public/unit/cluster_test.js
+++ frontEnd/public/unit/cluster_test.js
@@ -1,6 +1,19 @@
 'use strict'
 
+
 describe('Clusters', function(){
+
+	var ClusterService,
+		httpBackend;
+	
+	beforeEach(function() {
+    module('ClusterService');
+
+    inject(function($httpBackend, _ClusterService_) {
+      ClusterService = _ClusterService_;
+      httpBackend = $httpBackend;
+    });
+  });
 
 	it('Should be able to display.', function(){
 
@@ -25,5 +38,24 @@ describe('Clusters', function(){
 	it('Should be able to be sealed', function(){
 
 	});
+
+	it('Should require a cluster name', function(){
+		var dummy = ClusterService.save('name','descriptionTest');
+		var name = ClusterService.getClusterName(dummy);
+		expect(name).toEqual('name');
+	});
+
+	it('Should have a description', function(){
+		var dummy = ClusterService.save('name','descriptionTest');
+		var description = ClusterService.getDescription(dummy);
+		expect(description).toEqual('descriptionTest');
+
+	});
+
+	it('Form has refreshed/reset', function(){
+		var dummy = ClusterService.save('blah');
+		expect(dummy).toBeDefined();
+	});
+
 
 });
